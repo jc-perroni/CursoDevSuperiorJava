bd37fe19c1a00cfb38c7972206910c58a4f9932e
Julio Cesar Perroni <julio_veia@hotmail.com>
2024-07-20 15:32:18 -0300

    Fix: Adicionado getters e setters

--- contribuintes_oo_lista/src/main/java/entities/TaxPayer.java
+++ contribuintes_oo_lista/src/main/java/entities/TaxPayer.java
@@ -2,31 +2,71 @@ package entities;
 
 public class TaxPayer {
 
-private final double  salaryIncome;
-private final double servicesIncome;
-private final double capitalIncome;
-private final double healthSpending;
+private double salaryIncome;
+private double servicesIncome;
+private double capitalIncome;
+private double healthSpending;
 private final double educationSpending;
 private final double totalIncome;
 private final double totalSpendings;
 
-    public double getTotalIncome() {
-        return totalIncome;
-    }
+public double getTotalIncome() {
+    return totalIncome;
+}
 
-    public TaxPayer(double salaryIncome, double servicesIncome,
-                    double capitalIncome, double healthSpending,
-                    double educationSpending) {
-        this.salaryIncome = salaryIncome;
-        this.servicesIncome = servicesIncome;
-        this.capitalIncome = capitalIncome;
-        this.healthSpending = healthSpending;
-        this.educationSpending = educationSpending;
-        this.totalIncome = this.salaryIncome+this.servicesIncome+this.capitalIncome;
-        this.totalSpendings = this.healthSpending + this.educationSpending;
-    }
+public TaxPayer(double salaryIncome, double servicesIncome,
+                double capitalIncome, double healthSpending,
+                double educationSpending) {
+    this.salaryIncome = salaryIncome;
+    this.servicesIncome = servicesIncome;
+    this.capitalIncome = capitalIncome;
+    this.healthSpending = healthSpending;
+    this.educationSpending = educationSpending;
+    this.totalIncome = this.salaryIncome+this.servicesIncome+this.capitalIncome;
+    this.totalSpendings = this.healthSpending + this.educationSpending;
+}
 
-    public double salaryTax(){
+public double getSalaryIncome() {
+    return salaryIncome;
+}
+
+public void setSalaryIncome(double salaryIncome) {
+    this.salaryIncome = salaryIncome;
+}
+
+public double getServicesIncome() {
+    return servicesIncome;
+}
+
+public void setServicesIncome(double servicesIncome) {
+    this.servicesIncome = servicesIncome;
+}
+
+public double getCapitalIncome() {
+    return capitalIncome;
+}
+
+public void setCapitalIncome(double capitalIncome) {
+    this.capitalIncome = capitalIncome;
+}
+
+public double getHealthSpending() {
+    return healthSpending;
+}
+
+public void setHealthSpending(double healthSpending) {
+    this.healthSpending = healthSpending;
+}
+
+public double getEducationSpending() {
+    return educationSpending;
+}
+
+public double getTotalSpendings() {
+    return totalSpendings;
+}
+
+public double salaryTax(){
     if (this.salaryIncome/12 < 3000.00) {
         return 0;
     }
